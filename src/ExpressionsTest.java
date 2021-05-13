f91f14fbbca4d272805c8aa21981dbe7310f3914
David Bogoslavsky <bogo3333@gmail.com>
2021-05-13 11:52:12 +0300

    Test sequence changed, comments added

--- src/ExpressionsTest.java
+++ src/ExpressionsTest.java
@@ -34,20 +34,33 @@ public class ExpressionsTest {
         e3 = e3.assign("x", new Val(false));
         s = e3.toString();
         System.out.println(s);
+        Boolean value = e3.evaluate();
+        // At this point the expression looks like this: ~((T & (F | ((F & y) ^ T))) ^ F)
+        // Even though it contains an unassigned variable (y), the evaluation succeeds and
+        // doesn't throw an exception. It's because the compiler is smart enough not to
+        // call the evaluate() function for the "y" Var expression at all after the
+        // evaluate() method returned FALSE for the first subexpression of the
+        // And expression (F & y)
+        System.out.println("e3 evaluation result is: " + value);
 
         Expression e4 = new Or(new Var("v1"), new Var("v2"));
         s = e4.toString();
         System.out.println(s);
-/*      Boolean value = e4.evaluate();
-        // This throws an exception which is by design, since the values of v1 and v2
+/*      // If uncommented, this will throw an exception, which is by design since the values of v1 and v2
         // haven't been assigned
- */
+        value = e4.evaluate();
         System.out.println("e4 evaluation result is: " + value);
+*/
 
         Map<String, Boolean> assignment = new TreeMap<>();
         assignment.put("x", true);
         assignment.put("y", false);
         value = e2.evaluate(assignment);
         System.out.println("The result is: " + value);
+
+        Expression e5 = new Not(new Var("v"));
+        s = e5.toString();
+        // This demonstrates that we can add brackets around unary expressions that don't already have them
+        System.out.println(s);
     }
 }
